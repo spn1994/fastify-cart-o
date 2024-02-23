44589ed90ca976a4fd4c30e5139fbcbcdb023b06
Sergio <spn1994@hotmail.com>
2024-02-22 21:04:47 -0300

    removi arquivos errados

--- knexfile.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//devido ao erro SyntaxError: Cannot use import statement outside a module, mudei para assim
-const database_1 = require("./src/database");
-const config = database_1.config;
-exports.default = config;
